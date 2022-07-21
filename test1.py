11d1cccf82f9b6d905fd9ed0bbcab86856e7c4ee
Hariprasad5123 <annahariprasad5123@gmail.com>
2022-07-21 12:12:14 +0530

    mongodb practise

---
+++ test1.py
@@ -0,0 +1,17 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://root:system@cluster0.zhurd.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+details = {
+    "Surname" : "Anna",
+    "Firstname" : "Hari",
+    "Lastname" : "Prasad",
+    "email" : "annahari.ram97@gmail.com"
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(details)
+
